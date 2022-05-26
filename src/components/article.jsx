67c4169878458ff46c28e84e81d2b43a470597b6
coolaevv <coolaew9331@gmail.com>
2022-05-26 06:31:51 +0300

    uyhmyrhg

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -12,15 +12,32 @@ function Article(props) {
         <div className='carousel-post'>
             <Link to={'/news/' + news.id} className='news-link'>{news.title}</Link>
             <Carousel interval={null} fade={true}>
-                {news.pictures.map( (href, i) =>  
+                {news.pictures.map((href, i) =>
                     <Carousel.Item>
                         <img className="d-block w-100" src={href} alt={news.title} title={news.title} />
                     </Carousel.Item>
                 )}
             </Carousel>
             <p>{news.desc}</p>
+
+            <div className="post-options">
+                <div className="pubdate opt">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-calendar-event" viewBox="0 0 16 16">
+                        <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
+                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                    </svg>
+                    <span>{news.pubdate}</span>
+                </div>
+                <div className="views opt">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                    </svg>
+                    <span>{news.views}</span>
+                </div>
+            </div>
         </div>
-        
+
     )
 }
 
